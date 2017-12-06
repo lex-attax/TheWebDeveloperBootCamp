ebd076ef2fa2a8b6b6a3423d555c48b8dd6becff
lex-attax <lex.pavlovic@gmail.com>
2017-12-06 14:01:06 +0100

    Add files via upload

---
+++ Lesson07/L07-02-StalkerExercise/Script.js
@@ -0,0 +1,6 @@
+let firstName = prompt("What's your first name?")
+let lastName = prompt("What's your last name?")
+let age = prompt("How old are you?")
+
+console.log("Your first name is " + firstName + " " + lastName)
+console.log("You are " + age + " years old")
