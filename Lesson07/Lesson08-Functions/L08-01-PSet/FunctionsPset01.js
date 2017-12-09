0ba469bfc9c89b1f8c1f5d30b63c11f072fcb3a4
lex-attax <lex.pavlovic@gmail.com>
2017-12-09 20:51:49 +0100

    Add files via upload

---
+++ Lesson07/Lesson08-Functions/L08-01-PSet/FunctionsPset01.js
@@ -0,0 +1,8 @@
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
---
+++ Lesson07/Lesson08-Functions/L08-01-PSet/FunctionsPset03.js
@@ -0,0 +1,6 @@
+function kebabToSnake(str) {
+    //replace all '-' with '_'
+    let newStr = str.replace(/-/g, "_")
+    //return str
+    return newStr
+}
