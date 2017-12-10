12e95f0fec60fc642ae0494f95d2d944a2fab8ee
lex-attax <lex.pavlovic@gmail.com>
2017-12-10 21:30:46 +0100

    Add files via upload

---
+++ Lesson08-Functions/L08-02-ToDo-List/list.js
@@ -0,0 +1,42 @@
+var todos = ["Buy New Turtle"]
+
+var input = prompt("What would you like to do?")
+
+while (input !== "quit") {
+    //handle input
+    if (input == "list") {
+        listTodos()
+    } else if (input === "new") {
+        addTodo()
+    } else if (input === "delete") {
+        deleteTodo()
+    }
+    //ask again for new input
+    input = prompt("What would you like to do?")
+}
+console.log("OK, you quit the App")
+
+function listTodos() {
+    console.log("**********")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo)
+        console.log("**********")
+    })
+}
+
+function addTodo() {
+    //aks for new todo
+    var newTodo = prompt("Enter new todo")
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo")
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete")
+    //delete that todo
+    //splice()
+    todos.splice(index, 1)
+    console.log("Deleted Todo")
+}
