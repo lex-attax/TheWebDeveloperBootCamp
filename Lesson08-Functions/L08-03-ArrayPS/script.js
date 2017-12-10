02f56ae2f1ea98a21f4e7e338bc084e9d795c2c0
lex-attax <lex.pavlovic@gmail.com>
2017-12-10 21:31:23 +0100

    Add files via upload

---
+++ Lesson08-Functions/L08-03-ArrayPS/script.js
@@ -0,0 +1,17 @@
+function printReverse(array) {
+    for (var i = array.length; i >= 0; i--) {
+        console.log(array[i])
+    }
+}
+
+function isUniform(checkUni) {
+
+}
+
+function sumArray(input) {
+
+}
+
+function max(params) {
+
+}
