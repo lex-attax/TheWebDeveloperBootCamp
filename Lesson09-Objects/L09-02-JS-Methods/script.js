4383b6cbcb72c5f680612c8898fb0fd8ed26e0bd
lex-attax <lex.pavlovic@gmail.com>
2017-12-12 09:15:47 +0100

    Add files via upload

---
+++ Lesson09-Objects/L09-02-JS-Methods/script.js
@@ -0,0 +1,11 @@
+// Methods are Function inside of Objects
+
+var obj = {
+    name: "Chuck",
+    age: 45,
+    isCool: false,
+    friends: ["bob", "tina"],
+    add: function (x, y) {
+        return x + y;
+    }
+}
