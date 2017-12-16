83c29cd9686ab3b72d2589455090ae5ebc4b5fef
lex-attax <lex.pavlovic@gmail.com>
2017-12-16 18:26:58 +0100

    Add files via upload

--- Lesson10-DOM-Manipulation/L10-06-ColorGame/colorGame.js
+++ Lesson10-DOM-Manipulation/L10-06-ColorGame/colorGame.js
@@ -1,16 +1,10 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+let colors = generateRandomColors(6)
 
 let squares = document.querySelectorAll(".square")
 let pickedColor = pickColor()
 let colorDisplay = document.getElementById("colorDisplay")
 let messageDisplay = document.querySelector("#message")
+let h1 = document.querySelector("h1")
 
 
 colorDisplay.textContent = pickedColor
@@ -27,6 +21,7 @@ for (let i = 0; i < squares.length; i++) {
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor)
+            h1.style.backgroundColor = clickedColor
         } else {
             this.style.backgroundColor = "#232323"
             messageDisplay.textContent = "Try Again"
@@ -44,4 +39,26 @@ function changeColors(color) {
 function pickColor() {
     let random = Math.floor(Math.random() * colors.length)
     return colors[random]
+}
+
+function generateRandomColors(num) {
+    //make an array
+    let arr = []
+    //add num random colors to arr
+    for (let i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor())
+    }
+    //return that array
+    return arr
+}
+
+function randomColor() {
+    //pick a "red" from 0 to 255
+    let r = Math.floor(Math.random() * 256)
+    //pick a "green" from 0 to 255
+    let g = Math.floor(Math.random() * 256)
+    //pick a "blue" from 0 to 255
+    let b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + ", " + g + ", " + b + ")"
 }
