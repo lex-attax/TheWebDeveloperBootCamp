268d12a08da5adfbf870b5222b6c76bb63455e7b
lex-attax <lex.pavlovic@gmail.com>
2017-12-17 11:11:52 +0100

    Add files via upload

--- Lesson10-DOM-Manipulation/L10-06-ColorGame/colorGame.js
+++ Lesson10-DOM-Manipulation/L10-06-ColorGame/colorGame.js
@@ -46,11 +46,14 @@ resetButton.addEventListener("click", function () {
     pickedColor = pickColor()
     //change colorDisplay to match picked Color
     colorDisplay.textContent = pickedColor
+    this.textContent = "New Colors"
+
+    messageDisplay.textContent = "";
     //change colors of squares
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i]
     }
-    h1.style.background = "#232323"
+    h1.style.background = "steelblue"
 })
 
 colorDisplay.textContent = pickedColor
