e0de57300eb6698459d5f58f72e2c0aba770aa05
lex-attax <lex.pavlovic@gmail.com>
2017-12-19 09:18:58 +0100

    Add files via upload

--- Lesson12-ToDo-List/assets/js/todos.js
+++ Lesson12-ToDo-List/assets/js/todos.js
@@ -1,16 +1,22 @@
 // check off specific todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed")
 })
 
 //click on x to delete todos
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove()
     })
     event.stopPropagation()
 })
 
-$("input[type='text']").keypress(function () {
-    console.log("Key Press!")
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        let todoText = $(this).val()
+        $(this).val("")
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
 })
