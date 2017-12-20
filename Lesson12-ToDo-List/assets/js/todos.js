585260def7671da8a35b0bf48de14d18670ab5da
lex-attax <lex.pavlovic@gmail.com>
2017-12-20 08:17:03 +0100

    Add files via upload

--- Lesson12-ToDo-List/assets/js/todos.js
+++ Lesson12-ToDo-List/assets/js/todos.js
@@ -10,13 +10,14 @@ $("ul").on("click", "span", function (event) {
     })
     event.stopPropagation()
 })
-
+//add input to todo list
 $("input[type='text']").keypress(function (event) {
+    //if user presses the return-key
     if (event.which === 13) {
         //grabbing new todo text from input
         let todoText = $(this).val()
         $(this).val("")
         //create a new li and add to ul
-        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 })
