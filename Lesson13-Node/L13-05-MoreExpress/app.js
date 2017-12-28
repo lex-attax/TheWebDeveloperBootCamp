f1cf22ad9ffcc59b24d249fa438bef346c692e1d
lex-attax <lex.pavlovic@gmail.com>
2017-12-28 21:12:03 +0100

    Update app.js

--- Lesson13-Node/L13-05-MoreExpress/app.js
+++ Lesson13-Node/L13-05-MoreExpress/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing})
 })
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Colt"},
+        ]
+        
+        res.render("posts.ejs", {posts: posts})
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening!")
 })
