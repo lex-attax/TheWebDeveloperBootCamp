0c0ab9f188b3eaf916dadda04d73961032019882
lex-attax <lex.pavlovic@gmail.com>
2018-01-01 16:01:19 +0100

    Update app.js

--- Lesson14-APIs/L14-02-movieSearchApp/app.js
+++ Lesson14-APIs/L14-02-movieSearchApp/app.js
@@ -1,18 +1,25 @@
-var express = require("express")
-var app = express()
-var request = require("request")
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("search");
+});
 
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s?california", function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var results = JSON.parse(body)
-            res.send(results["Search"][0]["Title"])
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s=" + query;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("results", {data: data});
         }
-    })
-})
+    });
+});
 
 
-// Tell Express to listen for request (start server)
+
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server has started!")
-})
+    console.log("Movie App has started!!!");
+});
