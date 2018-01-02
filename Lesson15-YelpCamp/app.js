fe4b86b9fbdeaffd6e961399a416f6d6ee305021
lex-attax <lex.pavlovic@gmail.com>
2018-01-02 08:17:03 +0100

    Update app.js

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -1,20 +1,38 @@
 var express = require("express")
 var app = express()
+var bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
+        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
+        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"}
+        ]
 
 app.get("/", function(req, res){
     res.render("landing")
 })
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
-        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
-        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"}
-        ]
-        
+
         res.render("campgrounds",{campgrounds:campgrounds})
+})
+
+app.post("/campgrounds", function(req, res){
+
+    //get data from form and add to campgrunds array
+    var name = req.body.name
+    var image= req.body.image
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground)
+    //redirect back to campgrounds 
+    res.redirect("/campgrounds")
+})
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs")
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
