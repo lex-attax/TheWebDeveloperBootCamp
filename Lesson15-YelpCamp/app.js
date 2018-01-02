56fe6b1c6dcec7c61683bc7fffcae55e7a498e06
lex-attax <lex.pavlovic@gmail.com>
2018-01-02 11:44:19 +0100

    Update app.js

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs")
 var campgrounds = [
         {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
         {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
+        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"},
+        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
+        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
+        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"},
+        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
+        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
         {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"}
         ]
 
