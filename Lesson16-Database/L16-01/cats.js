a035e7c9d0559c7bf2789e9585db7853a0a176da
lex-attax <lex.pavlovic@gmail.com>
2018-01-03 07:38:24 +0100

    Create cats.js

---
+++ Lesson16-Database/L16-01/cats.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost/cat_app")
+
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+})
+ var Cat = mongoose.model("Cat", catSchema)
+
+// //adding a new cat to the DB
+
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// })
+
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("Something went wrong!")
+//     } else {
+//         console.log("We just saved a cat to the DB:")
+//         console.log(cat)
+//     }
+// })
+    
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function(err, cat){
+    if(err){
+        console.log(err)
+    } else {
+        console.log(cat)
+    }
+})
+
+//retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("ERROR!")
+        console.log(err)
+    } else {
+        console.log("all the cats...")
+        console.log(cats)
+    }
+    
+})
+
