5a646df99a4444e6dcc7960b93a915058cb2abae
lex-attax <lex.pavlovic@gmail.com>
2018-01-03 08:42:26 +0100

    set up mongoose + a new db

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -1,29 +1,47 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
-        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
-        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"},
-        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
-        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
-        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"},
-        {name: "Salmon Creek", image: "http://photosforclass.com/download/4684194306"},
-        {name: "Granite Hill", image: "http://photosforclass.com/download/2164766085"},
-        {name: "Mountain Goat's rest", image: "http://photosforclass.com/download/15215548990"}
-        ]
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema)
+
+// Campground.create(
+//     {
+//         name: "Mountain Goat's rest",
+//         image: "http://photosforclass.com/download/15215548990"
+        
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err)
+//         } else {
+//             console.log("newly created campground:")
+//             console.log(campground)
+//         }
+//     })
 
 app.get("/", function(req, res){
     res.render("landing")
 })
 
 app.get("/campgrounds", function(req, res){
-
-        res.render("campgrounds",{campgrounds:campgrounds})
+        //Get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err)
+            } else {
+                res.render("campgrounds",{campgrounds:allCampgrounds})
+            }
+        })
 })
 
 app.post("/campgrounds", function(req, res){
@@ -32,9 +50,15 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name
     var image= req.body.image
     var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground)
-    //redirect back to campgrounds 
-    res.redirect("/campgrounds")
+    //create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err)
+        } else {
+            //redirect back to campgrounds 
+            res.redirect("/campgrounds")
+        }
+    })
 })
 
 app.get("/campgrounds/new", function(req, res){
