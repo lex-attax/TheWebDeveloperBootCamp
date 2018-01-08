9a5acf1340225bcfb035ee189f004eb6741a9712
lex-attax <lex.pavlovic@gmail.com>
2018-01-08 21:08:37 +0100

    Update app.js

--- Lesson17-RESTfulRouting/L17-01-RESTfulBlogApp/app.js
+++ Lesson17-RESTfulRouting/L17-01-RESTfulBlogApp/app.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express()
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app")
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSanitizer())
 app.use(methodOverride("_method"))
 
 // MONGOOSE/MODEL CONFIG
@@ -45,6 +47,7 @@ app.get("/blogs/new", function(req, res){
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
     // create blog
+    req.body.blog.body = req.sanitizer( req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new")
@@ -80,6 +83,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitizer( req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs")
@@ -87,6 +91,19 @@ app.put("/blogs/:id", function(req, res){
             res.redirect("/blogs/" + req.params.id)
         }
     })
+})
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+        res.redirect("/blogs")
+    } else {
+        res.redirect("/blogs")
+    }
+  })
+    //redirect somewhere
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
