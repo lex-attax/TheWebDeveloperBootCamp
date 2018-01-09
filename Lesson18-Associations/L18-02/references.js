a514fd0cf445537fdfa5d9dc96ad4897e9fac3c2
lex-attax <lex.pavlovic@gmail.com>
2018-01-09 21:34:39 +0100

    Create references.js

---
+++ Lesson18-Associations/L18-02/references.js
@@ -0,0 +1,63 @@
+var mongoose = require("mongoose")
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/blog_demo_2", {useMongoClient: true})
+
+// post - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+})
+var Post = mongoose.model("Post", postSchema)
+
+// user - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ]
+})
+
+var User = mongoose.model("User", userSchema)
+
+// Post.create({
+//     title: "how to cook the best burger pt. 3",
+//     content: "asdfdfsdfhrthrshfghrhrthgf"
+// }, function(err, post){
+//     User.findOne({email : "bob@gmail.com"}, function(err, foundUser){
+//         if(err){
+//             console.log(err)
+//         } else {
+//             foundUser.posts.push(post)
+//             foundUser.save(function(err, data){
+//                 if(err){
+//                     console.log(err)
+//                 } else {
+//                     console.log(data)
+//                 }
+//             })
+//         }
+//     })
+// })
+
+
+// User.create({
+//     email: "bob@gmail.com",
+//     name: "Bob Belcher"
+// })
+
+
+
+// find user
+User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+    if(err){
+        console.log(err)
+    } else {
+        console.log(user)
+    }
+})
+
+//find all posts for that user
