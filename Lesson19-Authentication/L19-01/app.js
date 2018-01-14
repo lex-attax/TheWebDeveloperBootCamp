f26fd366a49c8cd9e228fb5abc337422ac7e4e92
lex-attax <lex.pavlovic@gmail.com>
2018-01-14 12:35:51 +0100

    Update app.js

--- Lesson19-Authentication/L19-01/app.js
+++ Lesson19-Authentication/L19-01/app.js
@@ -1,9 +1,29 @@
-var express = require("express")
-var mongoose = require("mongoose")
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    localStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
 mongoose.connect("mongodb://localhost/auth_demo_app")
 
 var app = express()
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(require("express-session")({
+    secret: "the secret sentence is secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// ROUTES
 
 app.get("/", function(req, res){
     res.render("home")
@@ -11,6 +31,26 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret")
+})
+
+// Auth Routes
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register")
+})
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render("register")
+        }
+        passport.authenticate("local"(req, res, function(){
+            res.redirect("/secret")
+        }))
+    })
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
