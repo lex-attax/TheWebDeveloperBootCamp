c8a6104e19200629c83d6c3ca29a343bfe4c1015
lex-attax <lex.pavlovic@gmail.com>
2018-01-14 21:37:21 +0100

    Update app.js

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -14,6 +14,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 seedDB()    
     
+// passport configuration
+app.use(require("express-session")({
+    secret: "once again rusty wins",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())    
+passport.deserializeUser(User.deserializeUser())    
+    
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -104,6 +116,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
     //connect new comment to campground
     //redirect campground show page
 })
+
+// auth routes
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register")
+})
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render("register")
+        } 
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds")
+        })
+    })
+})
+
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
