b8521e65a889a2f82a17aa7a2230b131aa200d07
lex-attax <lex.pavlovic@gmail.com>
2018-01-18 08:36:03 +0100

    added isLoggedin middleware
    only logged in users can create campgrounds now.

--- Lesson15-YelpCamp/routes/campgrounds.js
+++ Lesson15-YelpCamp/routes/campgrounds.js
@@ -15,12 +15,16 @@ router.get("/", function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn,  function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author: author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -33,7 +37,7 @@ router.post("/", function(req, res){
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
    res.render("campgrounds/new"); 
 });
 
@@ -50,5 +54,13 @@ router.get("/:id", function(req, res){
         }
     });
 })
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+}
 
 module.exports = router
