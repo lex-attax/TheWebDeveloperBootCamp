edad4322830007b270edee0ec1a79fd5d7e93bdd
lex-attax <lex.pavlovic@gmail.com>
2018-01-24 08:07:34 +0100

    + flash message for isLoggedIn middleware

--- Lesson15-YelpCamp/middleware/index.js
+++ Lesson15-YelpCamp/middleware/index.js
@@ -45,8 +45,8 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next()
     }
+    req.flash("error", "Please Login First!")
     res.redirect("/login")
 }
-
 
 module.exports = middlewareObj
