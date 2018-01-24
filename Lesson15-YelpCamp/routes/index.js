94fcaaceb9bd225ceceb7f59c364b6c277b9d36e
lex-attax <lex.pavlovic@gmail.com>
2018-01-24 09:15:52 +0100

    + success message

--- Lesson15-YelpCamp/routes/index.js
+++ Lesson15-YelpCamp/routes/index.js
@@ -28,7 +28,7 @@ router.post("/register", function(req, res){
 
 // show login form
 router.get("/login", function(req, res){
-    res.render("login", {message: req.flash("error")})
+    res.render("login")
 })
 
 // handling login logic
@@ -42,6 +42,7 @@ router.post("/login", passport.authenticate("local",
 // logout route
 router.get("/logout", function(req,res){
     req.logout()
+    req.flash("success", "Logged you out!")
     res.redirect("/campgrounds")
 })
 
