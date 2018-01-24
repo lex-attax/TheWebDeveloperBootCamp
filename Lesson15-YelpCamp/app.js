b6919937d09f0cba9a9fb7027039adda845c502d
lex-attax <lex.pavlovic@gmail.com>
2018-01-24 08:08:02 +0100

    require and use flash

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -4,19 +4,24 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds")
+    seedDB      = require("./seeds"),
+    flash       = require("connect-flash")
     
 //requring routes    
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp");
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/yelp_camp")
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"))
+app.use(flash())
 
 //seedDB()  //seed the database   
     
@@ -42,5 +47,5 @@ app.use("/campgrounds", campgroundRoutes)
 app.use("/campgrounds/:id/comments", commentRoutes)
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
-});
+   console.log("The YelpCamp Server Has Started!")
+})
