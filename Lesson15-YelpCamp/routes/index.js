e1aee3d68459aed7f748f33c977f8ad0180eb971
lex-attax <lex.pavlovic@gmail.com>
2018-02-01 13:24:07 +0100

    + error & success messages

--- Lesson15-YelpCamp/routes/index.js
+++ Lesson15-YelpCamp/routes/index.js
@@ -17,10 +17,12 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
+            req.flash("error", err.message)
             console.log(err)
             return res.render("register")
         } 
         passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Welcome to YelpCamp " + user.username)
             res.redirect("/campgrounds")
         })
     })
@@ -45,13 +47,5 @@ router.get("/logout", function(req,res){
     req.flash("success", "Logged you out!")
     res.redirect("/campgrounds")
 })
-
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect("/login")
-}
 
 module.exports = router
