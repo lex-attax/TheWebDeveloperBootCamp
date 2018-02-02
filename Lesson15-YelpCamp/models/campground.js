05d603229075180d332cb23ae83617785cbbb577
lex-attax <lex.pavlovic@gmail.com>
2018-02-02 10:03:59 +0100

    Update campground.js

--- Lesson15-YelpCamp/models/campground.js
+++ Lesson15-YelpCamp/models/campground.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose")
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
+   price: String,
    image: String,
    description: String,
    author: {
@@ -17,6 +18,6 @@ var campgroundSchema = new mongoose.Schema({
          ref: "Comment"
       }
       ]
-});
+})
 
-module.exports = mongoose.model("Campground", campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema)
