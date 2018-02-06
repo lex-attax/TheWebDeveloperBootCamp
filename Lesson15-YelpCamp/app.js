4290ef49a44a89cfbc67b898e0201993947d419e
lex-attax <lex.pavlovic@gmail.com>
2018-02-06 08:35:50 +0100

    add heroku DB

--- Lesson15-YelpCamp/app.js
+++ Lesson15-YelpCamp/app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp")
+//mongoose.connect("mongodb://localhost/yelp_camp")
+mongoose.connect("mongodb://lex:attax@ds125938.mlab.com:25938/yelpcamp")
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
