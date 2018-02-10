b6e632f1a7f28ea326b4ee8d7575015a303647c3
lex-attax <lex.pavlovic@gmail.com>
2018-02-10 15:45:20 +0100

    Add files via upload

---
+++ Lesson21-OOP/L21-01/script.js
@@ -0,0 +1,14 @@
+console.log("Connected!")
+function Dog(name, age){
+    this.name = name
+    this.age = age
+    this.bark = function(){
+        console.log(this.name + " just barked!")
+    }
+}
+
+var rusty = new Dog('Rusty', 3);
+var fido = new Dog('Fido', 1);
+
+rusty.bark()
+fido.bark()
